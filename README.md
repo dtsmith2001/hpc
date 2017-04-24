3c22f6ba3ac1bbc077948e8e944dec49dea08caa
Dale Smith <dtsmith@mindspring.com>
2017-04-24 10:38:17 -0400

    initial commit of example; add documentation

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # High Performance Computing Explorations using PETSc and SLEPc
 
-Example code to explore high performance computing using PETSc and SLEPc
+Example code to explore high performance computing using PETSc and SLEPc. Here are a few resources to get you started with MPI clusters.
+
+[Portable, Extensible Toolkit for Scientific Computation](http://www.mcs.anl.gov/petsc/index.html)
+
+[Scalable Library for Eigenvalue Problem Computations](http://slepc.upv.es/)
+
+Both libraries use MPI.
 
 [Running an MPI Program](http://mpitutorial.com/tutorials/mpi-hello-world/)
+
 [Running an MPI Program in a LAN](http://mpitutorial.com/tutorials/running-an-mpi-cluster-within-a-lan/)
+
 [Launching an Amazon EC2 MPI Cluster](http://mpitutorial.com/tutorials/launching-an-amazon-ec2-mpi-cluster/)
---
+++ src/example1.cpp
@@ -0,0 +1,68 @@
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+#include "slepcsvd.h"
+
+#include "get_rss.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::setprecision;
+
+string help("Create a random sparse or dense matrix.\n"
+    "Use\n\t-n-row # rows\n\t-n-cols # columns\n\t-sparsity float\n\t-mat_type mpidense for a dense matrix\n\t"
+            "-mat_type mpiaij for a sparse matrix\n\n");
+
+int main(int argc, char **args) {
+    PetscRandom rctx;
+    PetscScalar rnd;
+    PetscReal sparsity;
+    PetscInt istart, iend, i, n_row, n_col, max_count;
+    PetscBool hh = (PetscBool)0;
+    Mat A;
+
+    cout << "Initialize" << endl;
+    PetscInitialize(&argc, &args, NULL, help.c_str());
+
+    PetscOptionsGetBool(NULL, NULL, "-help", &hh, NULL);
+    if (hh) {
+        PetscFinalize();
+        exit(1);
+    }
+    PetscOptionsGetInt(NULL, NULL, "-max_features", &max_count, NULL);
+    PetscOptionsGetReal(NULL, NULL, "-sparsity", &sparsity, NULL);
+    PetscOptionsGetInt(NULL, NULL, "-n_row", &n_row, NULL);
+    PetscOptionsGetInt(NULL, NULL, "-n_col", &n_col, NULL);
+    cout << "Got options" << endl;
+
+    MatCreate(PETSC_COMM_WORLD, &A);
+
+    // matrix, rows_local, cols_local, rows_global, cols_global - let petsc decide local sizes
+    MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, n_row, n_col);
+    MatSetFromOptions(A);
+    MatSetUp(A);
+    MatGetOwnershipRange(A, &istart, &iend);
+
+    PetscRandomCreate(PETSC_COMM_WORLD, &rctx);
+    PetscRandomSetInterval(rctx, 0.0, 1.0);
+    cout << "Create and assemble matrix" << endl;
+    for (i = istart; i < iend; i++) {
+        PetscRandomGetValue(rctx, &rnd);
+        rnd = rnd > sparsity ? 0.0 : rnd;
+        rnd = max_count * rnd;
+        MatSetValues(A, 1, &i, 1, &i, &rnd, ADD_VALUES);
+    }
+    PetscRandomDestroy(&rctx);
+    MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
+    // perform computations while messages are in transit via MPI
+    MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
+    cout << "Assembled" << endl;
+
+    MatDestroy(&A);
+    PetscFinalize();
+
+    cout << "Peak RSS " << mine::getPeakRSSMb() << " Mb" << endl;
+}
